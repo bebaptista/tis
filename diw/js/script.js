50c3bdb3ab239009744ef45311b23d6b571e52ce
bebaptista <bernardo.baptista@sga.pucminas.br>
2016-11-21 20:40:43 -0200

    Add files via upload

--- diw/js/script.js
+++ diw/js/script.js
@@ -1,3 +1,4 @@
+/*
 var xmlhttp = new XMLHttpRequest();
 		
 function processarRequisicao() {
@@ -7,18 +8,16 @@ function processarRequisicao() {
 		var num = a.restify.rows.length;
 		var link = "";
 
-		htmlString += getFormattedTable();
+		htmlString = getFormattedTable();
 		
 		for (j = 0; j < num; j++) {
 			link = a.restify.rows[j].values.id.value;
 			// alterar os 'htmlString = htmlString +' para 'htmlString +=' -> é a mesma coisa e o code fica menor  
-			htmlString = htmlString + "<tbody><tr><td>"+ a.restify.rows[j].values.id.value + "</td>"+ "<td>" + a.restify.rows[j].values.nome.value +"</td>"+ "<td>" + a.restify.rows[j].values.responsavel.value +"</td>"+ "<td>" + a.restify.rows[j].values.cnpj.value +"</td>"+ "<td>" + a.restify.rows[j].values.tel.value +"</td>"+ "<td>" + a.restify.rows[j].values.email.value +"</td></tr></tbody>";
-			htmlString += "</table>";
-			htmlString = htmlString + '<button class="btn btn-warning" onclick="Modifica(' + link + ');">Editar</button>';
-			htmlString = htmlString + '<button class="btn btn-danger" onclick="ApagaRegistro(' + link + ');">Deletar</button>';
+			htmlString = htmlString + "<tbody><tr><td>"+ a.restify.rows[j].values.id.value + "</td>"+ "<td>" + a.restify.rows[j].values.nome.value +"</td>"+ "<td>" + a.restify.rows[j].values.responsavel.value +"</td>"+ "<td>" + a.restify.rows[j].values.cnpj.value +"</td>"+ "<td>" + a.restify.rows[j].values.tel.value +"</td>"+ "<td>" + a.restify.rows[j].values.email.value +"</td>";
+			htmlString = htmlString + '<td><button class="btn btn-warning" onclick="Modifica(' + link + ');">Editar</button></td>';
+			htmlString = htmlString + '<td><button class="btn btn-danger" onclick="ApagaRegistro(' + link + ');">Deletar</button></td></tr></tbody>';
 		}
-		//htmlString = htmlString + '<td><button onclick="ApagaRegistro(50);">Deletar</button></td></tr>';
-		
+		htmlString += "</table>";
 		document.getElementById("secao").innerHTML = htmlString;
 	}
 }
@@ -113,3 +112,126 @@ function getFormattedTable() {
 
 	return htmlString;
 }
+
+
+		function processarRequisicao() {
+			
+			if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+				var a = JSON.parse (xmlhttp.responseText);
+				var str = "";
+				var num = a.restify.rows.length;
+				var codigo = "", nome = "", email = "", telefone = "";
+				str = "<center><table class='table-bordered' border='2'></center><tr><th>CODIGO</th><th>Nome</th><th>Email</th><th>Telefone</th><th>Data do Cadastro</th></tr>"
+				for (j = 0; j < num; j++) {
+					codigo = a.restify.rows[j].values.codigo.value;
+					nome = a.restify.rows[j].values.nome.value;
+					email = a.restify.rows[j].values.email.value;
+					telefone = a.restify.rows[j].values.telefone.value;
+					str = str + "<tr><td>"+ codigo + "</td>"+ "<td>" + nome +"</td>"+ "<td>" + email +"</td>"+ "<td>" + telefone +"</td>"+ "<td>" + a.restify.rows[j].values.data_cadastro.value +"</td>";
+					str = str + '<td><button class="btn btn-warning" onclick="Modifica(\''+codigo+'\', \''+nome+'\', \''+email+'\', \''+telefone+'\');">Editar</button></td>';
+					str = str + "<td><button class='btn btn-danger' onclick='ApagaRegistro(\"" + codigo + "\");'>Deletar</button></td></tr>";
+				}
+				str = str + "</table>";
+				document.getElementById("secao").innerHTML = str;
+			}
+		}*/
+		
+		function ExecutaAjax() {	
+			var xmlhttp = new XMLHttpRequest();
+			xmlhttp.open("GET", "http://www.smartsoft.com.br/webservice/restifydb/Employees/diw_cadastro/?_view=json&_expand=yes", true);
+			xmlhttp.send();	
+			xmlhttp.onreadystatechange = function() {
+				if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+					var a = JSON.parse (xmlhttp.responseText);
+					var str = "";
+					var num = a.restify.rows.length;
+					var codigo = "", nome = "", email = "", telefone = "";
+					str = "<center><table class='table-bordered' border='2'></center><tr><th>CODIGO</th><th>Nome</th><th>Email</th><th>Telefone</th><th>Data do Cadastro</th></tr>"
+					for (j = 0; j < num; j++) {
+						codigo = a.restify.rows[j].values.codigo.value;
+						nome = a.restify.rows[j].values.nome.value;
+						email = a.restify.rows[j].values.email.value;
+						telefone = a.restify.rows[j].values.telefone.value;
+						str = str + "<tr><td>"+ codigo + "</td>"+ "<td>" + nome +"</td>"+ "<td>" + email +"</td>"+ "<td>" + telefone +"</td>"+ "<td>" + a.restify.rows[j].values.data_cadastro.value +"</td>";
+						str = str + '<td><button class="btn btn-warning" onclick="Modifica(\''+codigo+'\', \''+nome+'\', \''+email+'\', \''+telefone+'\');">Editar</button></td>';
+						str = str + "<td><button class='btn btn-danger' onclick='ApagaRegistro(\"" + codigo + "\");'>Deletar</button></td></tr>";
+					}
+					str = str + "</table>";
+					document.getElementById("secao").innerHTML = str;
+				}
+			}
+		}
+		
+		function ApagaRegistro(codigo){
+			if(confirm("Deseja apagar?")==true) {
+				var xmlhttp = new XMLHttpRequest();
+				xmlhttp.onreadystatechange=function() {
+					if (xmlhttp.readyState==4 && xmlhttp.status==200) {               
+						document.getElementById("teste").innerHTML = "Apagado com sucesso";
+					}
+				}
+				var link = "http://www.smartsoft.com.br/webservice/restifydb/Employees/diw_cadastro/";
+				var j=codigo;
+				link = link + j;
+				/*if(typeof codigo == 'number'){
+					var j = "";
+					j=codigo.toString();
+				}*/
+            	xmlhttp.open("DELETE", link, true);
+				xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');            
+				xmlhttp.send();		
+			}
+		}
+		
+		function InsereRegistro() {
+			if(confirm("Deseja inserir?")==true) {
+				var xmlhttp = new XMLHttpRequest();
+			
+				xmlhttp.onreadystatechange=function() {
+					if (xmlhttp.readyState==4 && xmlhttp.status==200) {               
+						document.getElementById("teste").innerHTML = xmlhttp.responseText;
+					}
+				}
+				var d = new Date();
+    			var timedate = d.toJSON();
+            	xmlhttp.open("POST", "http://www.smartsoft.com.br/webservice/restifydb/Employees/diw_cadastro", true);
+				xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');            
+				xmlhttp.send('_data={"nome":"'+document.getElementById('nome').value+'","email":"'+document.getElementById('email').value+'","data_cadastro":"'+timedate+'","telefone":"'+document.getElementById('telefone').value +'"}');
+			}
+		}
+		
+		function Insere() {
+			var str = "";
+			str = '<input id="nome" type="text" name="nome" placeholder="Nome"><input id="email" type="text" name="email" placeholder="Email"><input id="telefone" type="text" name="telefone" placeholder="Telefone"><button class="btn btn-default" onclick="InsereRegistro();">Enviar</button>'
+			document.getElementById("secao").innerHTML = str;	
+		}
+		
+		function Modifica(codigo,nome,email,telefone) {
+			var str = "";
+			str = '<input id="nome" type="text" name="nome" value='+nome+'><input id="email" type="text" name="email" value='+email+'><input id="telefone" type="text" name="telefone" value='+telefone+'><button class="btn btn-default" onclick="ModificaRegistro(\''+codigo+'\')">Enviar</button>'
+			document.getElementById("secao").innerHTML = str;
+		}
+		
+		function ModificaRegistro(codigo) {
+			if(confirm("Deseja modificar?")==true) {
+				var xmlhttp = new XMLHttpRequest();
+				
+				xmlhttp.onreadystatechange=function() {
+					if (xmlhttp.readyState==4 && xmlhttp.status==200) {          
+					document.getElementById("teste").innerHTML = xmlhttp.responseText;
+					}
+				}
+				var link = "http://www.smartsoft.com.br/webservice/restifydb/Employees/diw_cadastro/";
+				var j=codigo;
+				link = link + j;
+				/*if(typeof codigo == 'number'){
+					var j = "";
+					j=codigo.toString();
+				}*/
+				var d = new Date();
+    			var timedate = d.toJSON();
+            	xmlhttp.open("PUT", link, true);
+				xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');            
+				xmlhttp.send('_data={"nome":"'+document.getElementById('nome').value+'","email":"'+document.getElementById('email').value+'","data_cadastro":"'+timedate+'","telefone":"'+document.getElementById('telefone').value +'"}');
+			}
+		}
